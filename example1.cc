a641e8bbb4177e329f8045be662bb915b8d22ce9
Filip Czaplicki <fc359081@students.mimuw.edu.pl>
2015-11-22 16:34:02 +0100

    Example1 updated: TODOs why some testes doesn't work, tests currently working uncommented

--- example1.cc
+++ example1.cc
@@ -12,9 +12,9 @@ int main() {
 	{
 		VeryLongInt x = 100;
 		x -= 101;
-		//assert(!x.isValid());
+		assert(!x.isValid());
 	}
-	/*
+
 	{
 		VeryLongInt x = 23;
 		VeryLongInt y = x;
@@ -54,25 +54,31 @@ int main() {
 			assert(0);
 	}
 
-	{
+
+	//TODO: >>= operator needed
+	/*{
 		VeryLongInt x = 1;
 		x <<= 123;
 		x >>= 120;
 		assert(8 == x);
-	}
+	}*/
 
+	//TODO: *= operator and %= operator needed
+	/*
 	{
 		VeryLongInt x = 1;
 		for (int i = 1; i <= 100; ++i)
 			x *= 2;
 		assert(x % 3 == 1);
-	}
+	}*/
 
 	{
 		VeryLongInt x = Zero();
 		assert(x == 0);
 	}
 
+	//TODO: *= operator needed
+	/*
 	{
 		const int N = 100;
 		VeryLongInt x = 1;
@@ -80,22 +86,24 @@ int main() {
 			x *= 2;
 		assert(x.numberOfBinaryDigits() == N);
 	}
-*/
-	{
-		VeryLongInt x("1234567890123456789012345678901234567890");
-		std::cout << x << std::endl;
-		//VeryLongInt z = x;
-		//VeryLongInt y("777777777777777777777777777777777777777");
-//		x = x + y;
-//		x -= y;
-//		assert(x == z);
-	}
+	*/
 
+	//TODO: -= operator needed
 	/*{
+		VeryLongInt x("1234567890123456789012345678901234567890");
+		VeryLongInt z = x;
+		VeryLongInt y("777777777777777777777777777777777777777");
+		x = x + y;
+		x -= y;
+		assert(x == z);
+	}*/
+
+	{
 		VeryLongInt x(std::string("12345678"));
 		VeryLongInt y(12345678U);
 		assert(x == y);
 	}
+
 
 	{
 		VeryLongInt x("12345678901234567890");
@@ -103,37 +111,44 @@ int main() {
 		assert(x == y);
 		std::cout << y << std::endl;
 	}
-
+	//TODO: -= operator needed
+	/*
 	{
 		VeryLongInt x("1234567890123456789012345678901234567890");
 		VeryLongInt y("1204567890123456789012345678901234567890");
 		VeryLongInt z("30000000000000000000000000000000000000");
 		assert(z == x - y);
-	}
+	}*/
 
+	//TODO: *= operator needed
+	/*
 	{
 		VeryLongInt x("10000000000");
 		VeryLongInt y("100000000000");
 		VeryLongInt z("1000000000000000000000");
 		assert(z == x * y);
-	}
+	}*/
 
+	//TODO: *= operator and /= operator needed
+	/*
 	{
 		const int N = 1000;
 		VeryLongInt x = 1;
 		for (int i = 2; i <= N; ++i)
-
 			x *= i;
 		for (int i = 2; i <= N; ++i)
 			x /= i;
 		assert(x == 1);
 	}
+	*/
 
 	{
 		assert(Zero().numberOfBinaryDigits() == 1);
 		assert(NaN().numberOfBinaryDigits() == 0);
 	}
 
+	//TODO: *= operator needed
+	/*
 	{
 		VeryLongInt x(1);
 		VeryLongInt y(2);
